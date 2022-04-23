f632d2d1bd4155c624211cb84ec635def7ac04df
Kevnnm <63011171@kmitl.ac.th>
2022-04-23 21:48:58 +0700

    Injector process grabs and ungrabs devices
    The injector process that is spawned will now grabs all necessary
    devices in a group determined by whether the config to inject has a key
    code that needs to be remapped.

--- evremapper/daemon.py
+++ evremapper/daemon.py
@@ -2,11 +2,13 @@
 
 from pydbus import SystemBus
 from evremapper.logger import logger
-from evremapper.devices import _DeviceDetection, DeviceGroupEncoder
+from evremapper.devices import DevGroups
+from evremapper.injector import Injector
+from evremapper.config import Config
 
+import os
 import time
 import sys
-import json
 from multiprocessing import Pipe
 
 import gi
@@ -28,6 +30,11 @@ class Daemon:
                     </method>
                     <method name='refresh'>
                     </method>
+                    <method name='inject_device'>
+                        <arg type='s' name='device_key' direction='in'/>
+                        <arg type='s' name='config' direction='in'/>
+                        <arg type='b' name='status' direction='out'/>
+                    </method>
                 </interface>
             </node>
         """
@@ -36,6 +43,7 @@ class Daemon:
         # TODO Initialize structures here
         logger.debug("Creating daemon")
 
+        self.injectors = []
         self.refreshed_devices_at = 0
 
     def run(self):
@@ -60,12 +68,23 @@ class Daemon:
         if now - 10 > self.refreshed_devices_at:
             logger.debug("Refreshing device list due to time since last refresh")
             time.sleep(0.1)
-            # groups.refresh()
-            (r, w) = Pipe()
-            _DeviceDetection(w).start()
+            DevGroups.refresh()
 
-            result = r.recv()
             logger.debug("finished refreshing")
-            logger.debug(DeviceGroupEncoder().encode(result))
+            logger.debug("%s", [group.key for group in DevGroups])
             self.refreshed_devices_at = now
             return
+
+    def inject_device(self, device_key, config):
+        # TODO: Get rid of refresh dbus method and refresh when we cannot find the device to inject
+        logger.info('request to inject device "%s"', device_key)
+        inject_group = DevGroups.find(key=device_key)
+
+        config = Config.from_string(config)
+        logger.debug("config to inject: %s", config)
+
+        injector = Injector(inject_group, config)
+        self.injectors.append(injector)
+        injector.start()
+
+        return True
